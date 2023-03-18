8650f4dc33ca4de24b1a86e6bb872c78a25343ce
sam <samscloud2@gmail.com>
2023-03-18 01:10:45 -0300

    starting app from scratch. first cp build

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import React,{ Component } from "react";
+
+export class NavBar extends Component{
+    render(){
+        return (<>
+        <h1 className="btn btn-danger">Hello</h1>
+        <h1 className="btn btn-danger">Second Hello</h1>
+        </>)
+    }
+}
