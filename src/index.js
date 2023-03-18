0208445257590eb56b34e4443ce970c22c984188
sam <samscloud2@gmail.com>
2023-03-18 12:36:50 -0300

    added 2 components

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import "jquery";
 import "popper.js/dist/umd/popper" 
 import "bootstrap/dist/js/bootstrap";
 import "bootstrap/dist/css/bootstrap.css"; 
-import { NavBar } from "./App";
+import { App } from "./App";
 
 
-ReactDOM.render(<NavBar />, document.getElementById('root')) // renders element in the element with id named "root"
+ReactDOM.render(<App />, document.getElementById('root')) // renders element in the element with id named "root"
